7adf473e41035aa6296b51025b4e59f2ad4f4d8a
Brian Levin <bml201095@gmail.com>
2020-08-14 02:00:19 -0400

    added data variable which is going to hold the object values

---
+++ app.js
@@ -0,0 +1,11 @@
+const data = [
+    {
+      name: 'John Doe',
+      age: 32,
+      gender: 'male',
+      lookingfor: 'female',
+      location: 'Boston MA',
+      image: 'https://randomuser.me/api/portraits/men/82.jpg'
+    },
+
+]
